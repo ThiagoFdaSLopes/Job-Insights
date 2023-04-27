8899005dd64ab466ec1998a4d13710335faeaa8e
Thiago <programadorthiagolopes@gmail.com>
2023-04-27 19:07:02 -0300

    feat: Get Unique Jobs created

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -14,21 +14,12 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    data = read(path)
+    job_types = []
+    for row in data:
+        job_types.append(row["job_type"])
+    unique_job_types = list(set(job_types))
+    return unique_job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
