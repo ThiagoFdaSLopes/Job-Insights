c88a2e4869e3eb57864bb0113eb806b4ef93b089
Thiago <programadorthiagolopes@gmail.com>
2023-04-27 18:51:27 -0300

    feat: Criado logica funcao read

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,16 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    list = []
+    with open(path, encoding="utf-8") as file:
+        archive = csv.DictReader(file)
+        for row in archive:
+            list.append(row)
+    return list
 
 
 def get_unique_job_types(path: str) -> List[str]:
