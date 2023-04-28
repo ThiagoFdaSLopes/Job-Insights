21d6f6488c56894a1f1565628e0c43ee26d31d04
Thiago <programadorthiagolopes@gmail.com>
2023-04-27 21:40:04 -0300

    feat: Created Test Brazilian Jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    list_english = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert "title" in list_english[0]
+    assert "salary" in list_english[0]
+    assert "type" in list_english[0]
